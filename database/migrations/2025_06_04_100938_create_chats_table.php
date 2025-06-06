96abde2bd2ba437534a30a3b83fba3be114bc5d0
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2025-06-06 23:58:05 +0600

    Add migrations for conversations and chats tables with foreign key constraints

--- database/migrations/2025_06_04_100938_create_chats_table.php
+++ database/migrations/2025_06_04_100938_create_chats_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,10 @@ return new class extends Migration
     {
         Schema::create('chats', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('conversation_id')->constrained()->onDelete('cascade');
+            $table->foreignId('sender_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('receiver_id')->constrained('users')->onDelete('cascade');
+            $table->text('message');
             $table->timestamps();
         });
     }
