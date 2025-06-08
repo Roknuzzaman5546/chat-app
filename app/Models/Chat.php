294acf8e95bf2ef1bb1d4b34f844ca4fc380b9ef
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2025-06-08 10:40:55 +0600

    Chat and conversation model code added

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chat extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['conversation_id', 'sender_id', 'receiver_id', 'message'];
+
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
 }
--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Conversation extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['user_one_id', 'user_two_id'];
+
+    public function chats()
+    {
+        return $this->hasMany(Chat::class);
+    }
+
+    public function userOne()
+    {
+        return $this->belongsTo(User::class, 'user_one_id');
+    }
+
+    public function userTwo()
+    {
+        return $this->belongsTo(User::class, 'user_two_id');
+    }
 }
+
